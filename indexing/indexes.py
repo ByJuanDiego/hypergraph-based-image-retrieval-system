39fdd47d0863a3d5e45eccee0dbdf3889ff660e0
juan-diego <juan.castro.p@utec.edu.pe>
2023-11-20 10:21:32 -0500

    HyperGraph structure advances

--- indexing/indexes.py
+++ indexing/indexes.py
@@ -1,11 +1,11 @@
 from graph.essential import Graph, Cluster
-from typing import List, Callable
-
+from typing import List, Callable, Tuple
 import pickle
 
 
 class HyperGraph:
     _clusters: List[Cluster] = []
+    _overlapping: List[List[Tuple[int, float]]]
     _graphs: List[Graph]
     _centroids: List[Graph]
     _distance: Callable[[Graph, Graph], float]
@@ -33,12 +33,32 @@ class HyperGraph:
             cluster.set_centroid(centroid)
             self._clusters.append(cluster)
 
-        for cluster in self._clusters:
-            for graph in self._graphs:
+        for graph in self._graphs:
+            for cluster in self._clusters:
                 if graph.get_path() == cluster.get_centroid().get_path():
                     continue
                 if self._distance(graph, cluster.get_centroid()) < self._threshold:
                     cluster.add_graph(graph)
+
+        # Overlapping measurements (for hyper-graph navigation purposes)
+
+        print("overlap")
+        n = len(self._clusters)
+        self._overlapping = [[] for _ in range(n)]
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                c1_paths = [graph.get_path() for graph in self._clusters[i].get_graphs()]
+                c2_paths = [graph.get_path() for graph in self._clusters[j].get_graphs()]
+
+                overlap = len([path for path in c1_paths if path in c2_paths])
+
+                if overlap > 0:
+                    self._overlapping[i].append((j, overlap))
+                    self._overlapping[j].append((i, overlap))
+
+        for i in range(n):
+            self._overlapping[i].sort(key=lambda x: x[1])
 
     def get_clusters(
             self
@@ -56,7 +76,8 @@ class HyperGraph:
             filename
     ) -> None:
         with open(dir_path + "/" + filename, "wb") as file:
-            pickle.dump(self.get_clusters(), file)
+            data: Tuple[List[Cluster], List[List[Tuple[int, float]]]] = (self._clusters, self._overlapping)
+            pickle.dump(data, file)
 
     def load_clusters(
             self,
@@ -64,11 +85,20 @@ class HyperGraph:
             filename
     ) -> None:
         with open(dir_path + "/" + filename, "rb") as file:
-            self._clusters = pickle.load(file)
+            data: Tuple[List[Cluster], List[List[Tuple[int, float]]]] = pickle.load(file)
+            self._clusters = data[0]
+            self._overlapping = data[1]
 
     def pretty_print(
             self
     ) -> None:
-        for cluster in self.get_clusters()[20:30]:
+        for cluster in self.get_clusters():
+
             print(f"{cluster.get_centroid().get_path()}")
             print(f"{list(map(lambda graph: graph.get_path(), cluster.get_graphs()))}")
+            print()
+
+        #
+        # for x in self._overlapping:
+        #     if x:
+        #         print(x[len(x) - 1])
--- main.py
+++ main.py
@@ -9,18 +9,24 @@ graphs: List[Graph] = load_dataset_in_batches("pickles/graphs")
 
 threshold = 20
 
-mean_shift = MeanShift(graphs, weighted_distance, threshold)
-mean_shift.fit()
-mean_shift.save_centroids("pickles/20", "centroids.p")
+# mean_shift = MeanShift(graphs, weighted_distance, threshold)
 
-centroids = mean_shift.get_centroids()
+# mean_shift.fit()
+# mean_shift.save_centroids("pickles/20", "centroids.p")
 
-hypergraph = HyperGraph(graphs, centroids, weighted_distance, threshold)
-hypergraph.fit()
-hypergraph.save_clusters("pickles/20", "clusters.p")
+# mean_shift.load_centroids("pickles/20", "centroids.p")
+# centroids = mean_shift.get_centroids()
+
+# hypergraph = HyperGraph(graphs, centroids, weighted_distance, threshold)
+# hypergraph.fit()
+# hypergraph.save_clusters("pickles/20", "clusters.p")
+#
+# hypergraph.pretty_print()
+
+
+hypergraph = HyperGraph(graphs, centroids=[], distance=weighted_distance, threshold=threshold)
+hypergraph.load_clusters("pickles/20", "clusters.p")
+# hypergraph.save_clusters("pickles/20", "clusters.p")
+# hypergraph.load_clusters("pickles/20", "clusters.p")
 
 hypergraph.pretty_print()
-
-# hypergraph = HyperGraph(graphs, centroids=[], distance=weighted_distance, threshold=threshold)
-# hypergraph.load_clusters("pickles/20", "clusters.p")
-# hypergraph.pretty_print()
